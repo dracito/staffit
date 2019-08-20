2a9aff5f8fc832034e3a6f3e3393e61244eead47
dracito <dracito@gmail.com>
2019-08-20 14:28:45 +0200

    Added priority & fix

--- src/app/status-list/status-list.component.ts
+++ src/app/status-list/status-list.component.ts
@@ -12,19 +12,20 @@ export class StatusListComponent {
   private gridColumnApi;
   private columnDefs = [
         {headerName: 'Status', field: 'status', sortable: true, filter: true },
-        {headerName: 'Piste', field: 'piste', sortable: true, filter: true },
+        {headerName: 'Piste', field: 'piste', sortable: true, filter: true },        
+        {headerName: 'Priorité', field: 'priority', sortable: true, filter: true },
         {headerName: 'Nom', field: 'name', sortable: true, filter: true },
         {headerName: 'Prénom', field: 'firstname', sortable: true, filter: true },
-        {headerName: 'Métiers', field: 'metiers', sortable: true, filter: true },
+        {headerName: 'Métiers', field: 'skills', sortable: true, filter: true },
         {headerName: 'Exp.', field: 'xp', sortable: true, filter: true },
-        {headerName: 'Dispo le', field: 'dispo', sortable: true, filter: true },
+        {headerName: 'Disponible le', field: 'availability', sortable: true, filter: true },
         {headerName: 'Action', field: 'action'}
     ];
 
     private rowData = [
-        { status: 'Préqualifier', name: 'DOE', firstname: "John", metiers: "C++", xp: "1", dispo: "1/08/2019", piste: "Projet A", action: "" },
-        { status: 'Faire PDC A', name: 'MARTIN', firstname: "Jean", metiers: "JEE", xp: "3", dispo: "1/09/2019", piste: "Projet B", action: "" },
-        { status: 'Cloturer succès', name: 'DURAND', firstname: "Cécile", metiers: "SwARC", xp: "8", dispo: "1/10/2019", piste: "Projet A", action: "" },
+        { status: 'Préqualifier', name: 'DOE', firstname: "John", skills: "C++", xp: "1", availability: "1/08/2019", piste: "Projet A", priority:"P0", action: "" },
+        { status: 'Faire PDC A', name: 'MARTIN', firstname: "Jean", skills: "JEE", xp: "3", availability: "1/09/2019", piste: "Projet B", action: "" },
+        { status: 'Cloturer succès', name: 'DURAND', firstname: "Cécile", skills: "SwARC", xp: "8", availability: "1/10/2019", piste: "Projet A", priority:"P1", action: "" },
     ];
   autoSizeAll() {
     var allColumnIds = [];
